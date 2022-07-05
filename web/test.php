6ad8d9dcfc9f184c6f398af891e39752c53d902c
Aseerah2604 <aseerah786@gmail.com>
2022-07-05 13:14:44 +0800

    test

--- web/test.php
+++ web/test.php
@@ -1,7 +1,16 @@
+<!-- solved: page does not have the HTML doc type -->
+
+<!DOCTYPE html>
+
+<!-- solved: <html> element does not have a [lang] attribute -->
 <html lang="en">
 
 <head>
+<!-- solved: Does not have a <meta name="viewport"> tag with width or initial-scale -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- solved: Does not have a meta description -->
+<meta name="description" content="Description of the web page.">
+
 <title> Testing this page </title>
 
 </head>
